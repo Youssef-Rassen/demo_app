1fd4d83c4f4b8f86a46980e903b3ec9717e282e4
youssef <youssefrassen177@yahoo.com>
2023-10-21 16:47:28 +0300

    demo_app

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:demo_app_using_flutter/keys/keys.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Internals'),
+        ),
+        body: Keys(),
+      ),
+    );
+  }
+}
